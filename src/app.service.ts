b99b3e88158e625db44c557d9b03146667b445d4
LeeHyeonHee <ddrtocna@gmail.com>
2022-09-29 23:31:35 +0900

    add to scheduler

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,24 @@ import { ClientProxy } from '@nestjs/microservices';
 import { Cron } from '@nestjs/schedule';
 import { Controller, Inject, Injectable } from '@nestjs/common';
 
-@Injectable()
-@Controller()
 export class AppService {
   constructor(@Inject('MQTT_SERVICE') private client: ClientProxy) {}
-  getNotificationsAlarm() {
-    const randomNumber = String(Math.floor(Math.random() * 100000)).padStart(
-      1024,
-      '0',
-    );
-    const msg = this.client.send('notification_channel', randomNumber);
 
-    return msg;
+  @Cron('* * * * * *', { name: 'RNGTask' })
+  getNotificationsAlarm() {
+    const powerNumber = 10 ** 16;
+    [...new Array(64)].map((_, idx) => {
+      const randomNumber = String(
+        Math.floor(Math.random() * powerNumber),
+      ).padEnd(32, '0');
+      const topic = 'notification_channel';
+      this.client.emit(`${topic}${idx}`, {
+        device: idx,
+        topic: topic + idx,
+        data: randomNumber,
+      });
+    });
+    return 1;
   }
 
   getHello(): string {
